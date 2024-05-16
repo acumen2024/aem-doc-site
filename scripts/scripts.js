75a3b63f63a084466f322710f4ed0fcce4456362
acumen2024 <155452411+acumen2024@users.noreply.github.com>
2024-05-16 13:27:20 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,16 +29,6 @@ import {
   addSoundcloudShowHandler,
 } from './video-helper.js';
 
-const pluginContext = {
-  getAllMetadata,
-  getMetadata,
-  loadCSS,
-  loadScript,
-  sampleRUM,
-  toCamelCase,
-  toClassName,
-};
-
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 function getCTAContainer(ctaLink) {
